5ddeddc5e397a0ade45f6e787d910df4742ebd7b
Vivian Vu <vivianvu@mit.edu>
2020-07-05 18:59:42 -0400

    Changes to test testing optimization.

--- samri/pipelines/tests/test_preprocessing.py
+++ samri/pipelines/tests/test_preprocessing.py
@@ -1,7 +1,7 @@
 from samri.pipelines.preprocess import generic, legacy
 from samri.pipelines import manipulations
 
-BIDS_BASE = '/usr/share/samri_bidsdata/bids'
+BIDS_BASE = '/home/vivian/samri_bidsdata/bids'
 
 # Takes too long
 def test_generic():
@@ -10,6 +10,6 @@ def test_generic():
 		registration_mask='/usr/share/mouse-brain-atlases/dsurqec_200micron_mask.nii',
 		functional_match={'acquisition':['EPIlowcov'],},
 		structural_match={'acquisition':['TurboRARElowcov'],},
-		out_base='/var/tmp/samri_testing/pytest/',
+		out_base='/home/vivian/samri_bidsdata/bids/rs_preprocess/',
 		workflow_name='prep',
 		)
