1b8d48b672a383bbce51250b1fcf6ccf841cd829
Vivian Vu <vivianvu@mit.edu>
2020-07-26 14:01:19 -0400

    Uncommented tests.

--- samri/pipelines/tests/test_glm.py
+++ samri/pipelines/tests/test_glm.py
@@ -11,25 +11,27 @@ def test_l1():
 		)
 
 # Takes too long or hangs
-#def test_physio():
-#	l1_physio(PREPROCESS_BASE, 'astrocytes',
-#		highpass_sigma=180,
-#		convolution=False,
-#		mask='mouse',
-#		n_jobs_percentage=.33,
-#		match={
-#			'modality':['bold'],
-#			'session':['ofM'],
-#			},
-#		invert=False,
-#		workflow_name='l1_astrocytes',
-#		out_base='/var/tmp/samri_testing/pytest/',
-#		)
+def test_physio():
+	l1_physio(PREPROCESS_BASE, 'astrocytes',
+		highpass_sigma=180,
+		convolution=False,
+		mask='mouse',
+		n_jobs_percentage=.33,
+		match={
+			'modality':['bold'],
+			'session':['ofM'],
+			},
+		invert=False,
+		workflow_name='l1_astrocytes',
+		out_base='/var/tmp/samri_testing/pytest/',
+                keep_work=True,
+		)
 
 # Takes too long or hangs
-#def test_seed():
-#	seed(PREPROCESS_BASE,'/usr/share/mouse-brain-atlases/dsurqec_200micron_roi-dr.nii',
-#		match={"acq":["EPIlowcov"]},
-#		out_base='/var/tmp/samri_testing/pytest/',
-#		workflow_name='dr_fc',
-#		)
+def test_seed():
+	seed(PREPROCESS_BASE,'/usr/share/mouse-brain-atlases/dsurqec_200micron_roi-dr.nii',
+		match={"acq":["EPIlowcov"]},
+		out_base='/var/tmp/samri_testing/pytest/',
+		workflow_name='dr_fc',
+                keep_work=True,
+		)
